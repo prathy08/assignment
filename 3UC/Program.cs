ef46da640e377fe87a9d8c6c1519a32a0c5e6f7b
prathy08 <bashaboinaprathyusha08@gmail.com>
2023-02-17 13:45:13 +0530

    Add:assignment

---
+++ 3UC/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _3UC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             
+            {
+                 
+                 int IS_FULL_TIME = 1;
+                 int EMP_RATE_PER_HOUR = 20;
+                 int emphrs = 0;
+                int empwages = 0;
+                Random random = new Random();
+                int empCheck = random.Next(0, 2);
+                if (empCheck == IS_FULL_TIME)
+                {
+                    emphrs = 8;
+
+                }
+                else
+                {
+                    emphrs = 0;
+
+                }
+                empwages = emphrs * EMP_RATE_PER_HOUR;
+                Console.WriteLine("Emp wage :" + empwages);
+                
+                            
+              
+    }
+
+
+        }
+    }
+}
